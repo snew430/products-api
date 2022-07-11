74c23267229cba22797fed44474879b8dc9306f6
snew430 <seanpnew@hotmail.com>
2022-07-11 13:11:45 -0400

    added schema setups in swagger

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const routes = require('./controllers');
+const express = require("express");
+const routes = require("./controllers");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger.json");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const swaggerOptions = {
+  defaultModelsExpandDepth: -1,
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.use(
+  "/docs",
+  swaggerUi.serveFiles(swaggerDoc, { swaggerOptions }),
+  swaggerUi.setup(swaggerDoc)
+);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
